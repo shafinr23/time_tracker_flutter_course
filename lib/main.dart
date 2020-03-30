dc6171ec079e7051ff7ca3d995975ad653382b58
shafinr23@gmail.com <shafinr23@gmail.com>
2020-03-30 21:14:11 +0600

    addding authprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/landing_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
+import 'package:time_tracker_flutter_course/services/auth_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +10,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'time tracker',
-      theme: ThemeData(
-        primaryColor: Colors.brown,
-      ),
-      home: LandingPage(
-        auth: Auth(),
+    return AuthProvider(
+      auth: Auth(),
+      child: MaterialApp(
+        title: 'time tracker',
+        theme: ThemeData(
+          primaryColor: Colors.brown,
+        ),
+        home: LandingPage(
+          auth: Auth(),
+        ),
       ),
     );
   }
