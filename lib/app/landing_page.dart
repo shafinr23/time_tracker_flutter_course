1096418ba1e25fe1585a3d23a2d40243e9204a05
shafinr23@gmail.com <shafinr23@gmail.com>
2020-03-31 19:08:06 +0600

    add provider in the auth project

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter_course/app/home_page.dart';
 import 'package:time_tracker_flutter_course/app/sing_in/sing_in_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
-import 'package:time_tracker_flutter_course/services/auth_provider.dart';
 
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final auth = AuthProvider.of(context);
+    final auth = Provider.of<AuthBase>(context, listen: false);
     return StreamBuilder<User>(
       stream: auth.onAuthStateChange,
       builder: (context, snapshot) {
