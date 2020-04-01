c36ec4f1966db173cf5a411ed1e9d1ea439ff895
shafinr23@gmail.com <shafinr23@gmail.com>
2020-04-01 23:04:26 +0600

    add authentication in bloc

--- lib/app/sing_in/sing_in_bloc.dart
+++ lib/app/sing_in/sing_in_bloc.dart
@@ -1,11 +1,34 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
 class SingInBloc {
+  SingInBloc({@required this.auth});
+
+  final AuthBase auth;
   final StreamController<bool> _isLoadingController = StreamController<bool>();
+
   Stream<bool> get isLoadingStream => _isLoadingController.stream;
+
   void dispose() {
     _isLoadingController.close();
   }
 
-  void setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+
+  Future<User> _singIn(Future<User> Function() singInMathod) async {
+    try {
+      _setIsLoading(true);
+      return await singInMathod();
+    } catch (e) {
+      rethrow;
+    } finally {
+      _setIsLoading(false);
+    }
+  }
+
+  Future<User> singInAnonymosly() async => await _singIn(auth.singInAnonymosly);
+
+  Future<User> singInWithGoogle() async => await _singIn(auth.singInWithGoogle);
 }
