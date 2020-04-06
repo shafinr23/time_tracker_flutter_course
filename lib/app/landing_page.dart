3b255601d1637b2bb33de838862f75637f46d396
shafinr23@gmail.com <shafinr23@gmail.com>
2020-04-06 23:10:30 +0600

    adding provider

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter_course/app/home/jobs_page.dart';
 import 'package:time_tracker_flutter_course/app/sing_in/sing_in_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
+import 'package:time_tracker_flutter_course/services/database.dart';
 
 class LandingPage extends StatelessWidget {
   @override
@@ -16,7 +17,9 @@ class LandingPage extends StatelessWidget {
           if (user == null) {
             return SingInPage.create(context);
           }
-          return JobsPage();
+          return Provider<Database>(
+              create: (_) => FirestoreDatabase(uid: user.uid),
+              child: JobsPage());
         } else {
           return Scaffold(
             body: Center(
