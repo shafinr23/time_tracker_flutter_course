2a13d890526d002f13d3f64ce663d426b00642fd
shafinr23@gmail.com <shafinr23@gmail.com>
2020-04-07 23:06:24 +0600

    make database class

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,8 +1,18 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
 
-abstract class Database {}
+abstract class Database {
+  Future<void> createJob(Map<String, dynamic> jobData);
+}
 
 class FirestoreDatabase implements Database {
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
   final String uid;
+  Future<void> createJob(Map<String, dynamic> jobData) async {
+    final path = '/users/$uid/jobs/jobs_abc';
+    final docRef = Firestore.instance.document(path);
+    await docRef.setData(jobData);
+  }
 }
