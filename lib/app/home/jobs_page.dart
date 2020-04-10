36f769349646c9b2d6dffbddda1d6161f8b75ddc
shafinr23@gmail.com <shafinr23@gmail.com>
2020-04-10 22:37:35 +0600

    adding StreamBuilder

--- lib/app/home/jobs_page.dart
+++ lib/app/home/jobs_page.dart
@@ -45,9 +45,6 @@ class JobsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //TODO: temporary
-    final database = Provider.of<Database>(context);
-    database.jobsStream();
     return Scaffold(
       appBar: AppBar(
         title: Text('Jobs'),
@@ -62,10 +59,30 @@ class JobsPage extends StatelessWidget {
           )
         ],
       ),
+      body: Container(
+        child: _buildContents(context),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () => _createJob(context),
       ),
+    );
+  }
+
+  Widget _buildContents(BuildContext context) {
+    final database = Provider.of<Database>(context);
+    return StreamBuilder<List<Job>>(
+      stream: database.jobsStream(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final jobs = snapshot.data;
+          final children = jobs.map((job) => Text(job.name)).toList();
+          return ListView(children: children);
+        }
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
