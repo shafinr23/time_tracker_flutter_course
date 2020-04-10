3c45e09255a897b50fd4a1c9b3a0ed19a6cf90c0
shafinr23@gmail.com <shafinr23@gmail.com>
2020-04-10 21:26:28 +0600

    make a path to read

--- lib/services/api_path.dart
+++ lib/services/api_path.dart
@@ -1,3 +1,4 @@
 class APIPath {
-  static String job(String uid, String jobId) => '/users/$uid/jobs/$jobId';
+  static String job(String uid, String jobId) => 'users/$uid/jobs/$jobId';
+  static String Jobs(String uid) => 'users/$uid/jobs';
 }
