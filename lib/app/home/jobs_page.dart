9d32941e87fc0a6f37d094f3b2d0ea94e74f0699
shafinr23@gmail.com <shafinr23@gmail.com>
2020-04-10 21:26:42 +0600

    add read database class

--- lib/app/home/jobs_page.dart
+++ lib/app/home/jobs_page.dart
@@ -45,6 +45,9 @@ class JobsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //TODO: temporary
+    final database = Provider.of<Database>(context);
+    database.readJobs();
     return Scaffold(
       appBar: AppBar(
         title: Text('Jobs'),
