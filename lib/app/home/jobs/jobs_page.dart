a4594614155ed35ba2b837bb37ef517a85f17fc6
shafinr23@gmail.com <shafinr23@gmail.com>
2020-04-14 20:42:01 +0600

    intro placeholder widget

--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter_course/app/home/jobs/add_job_page.dart';
 import 'package:time_tracker_flutter_course/app/home/models/job.dart';
 import 'package:time_tracker_flutter_course/common_widgets/platform_alart_dialog.dart';
-import 'package:time_tracker_flutter_course/common_widgets/platform_exception_alert_dialog.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 import 'package:time_tracker_flutter_course/services/database.dart';
 
@@ -28,19 +26,6 @@ class JobsPage extends StatelessWidget {
         .show(context);
     if (didRequestSingout == true) {
       _singOut(context);
-    }
-  }
-
-  Future<void> _createJob(BuildContext context) async {
-    try {
-      final database = Provider.of<Database>(context, listen: false);
-
-      await database.createJob(Job(name: 'blogging', ratePerHour: 45));
-    } on PlatformException catch (e) {
-      PlatformExceptionAlertDialog(
-        title: 'opration failed',
-        exception: e,
-      ).show(context);
     }
   }
 
