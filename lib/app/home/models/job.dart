37fcf5205ea3e00e10fa1d270d2839e635e5188e
shafinr23@gmail.com <shafinr23@gmail.com>
2020-04-17 21:16:51 +0600

    get documentId for one step return form collection to document

--- lib/app/home/models/job.dart
+++ lib/app/home/models/job.dart
@@ -1,17 +1,18 @@
 import 'package:meta/meta.dart';
 
 class Job {
-  Job({@required this.name, @required this.ratePerHour});
+  Job({@required this.id, @required this.name, @required this.ratePerHour});
+  final String id;
   final String name;
   final int ratePerHour;
 
-  factory Job.frommap(Map<String, dynamic> data) {
+  factory Job.frommap(Map<String, dynamic> data, String documentId) {
     if (data == null) {
       return null;
     }
     final String name = data['name'];
     final int ratePerHour = data['ratePerHour'];
-    return Job(name: name, ratePerHour: ratePerHour);
+    return Job(name: name, ratePerHour: ratePerHour, id: documentId);
   }
   Map<String, dynamic> toMap() {
     return {
